49c2520961f138299b579203c3ba7a69f281b29b
Andrey Urusov <drobus@gmail.com>
2015-11-24 12:51:56 +0300

    Map -> Area, удален Item, переименован self, упрощение кода

--- python/a_star.py
+++ python/a_star.py
@@ -1,23 +1,17 @@
-class Map(object):
+class Coordinate(object):
+    def __init__(coordinate, x, y):
+        coordinate.x = x
+        coordinate.y = y
 
-    class Coordinate(object):
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
+    def __eq__(point1, point2):
+        return point1.x == point2.x and point1.y == point2.y
 
-        def __eq__(point1, point2):
-            return point1.x == point2.x and point1.y == point2.y
+    def getLength(point1, point2):
+        return abs(point1.x - point2.x) + abs(point1.y - point2.y)
 
-        def getLength(point1, point2):
-            return abs(point1.x - point2.x) + abs(point1.y - point2.y)
-
-    class Item(Coordinate):
-        def __init__(self, x, y, passability):
-            self.coordinate = Map.Coordinate(x, y)
-            self.passability = passability
-
-    def __init__(self):
-        passabilities = [
+class Area(object):
+    def __init__(area):
+        area.passabilities = [
             [1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
             [1, -1, -1, -1, -1, -1, -1, -1, -1,  1],
             [1,  1,  1,  1,  1,  1,  1,  1, -1,  1],
@@ -29,81 +23,84 @@ class Map(object):
             [1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
             [1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
         ]
+        area.widht = len(area.passabilities[0])
+        area.height = len(area.passabilities)
 
-        self.__map__ = []
-        y = 0;
-        for line in passabilities:
-            x = 0;
-            items = []
-            for passability in line:
-                items.append(Map.Item(x, y, passability))
-                x += 1
-            self.__map__.append(items)
-            y += 1
-        self.widht = len(self.__map__[0])
-        self.heidth = len(self.__map__)
+    def getPassability(area, coordinate):
+        return area.passabilities[coordinate.y][coordinate.x];
 
-    def get_str(self, coordinate_from, coordinate_to, path):
+    def toString(area, coordinate_from, coordinate_to, path):
         result = ''
-        for line in self.__map__:
+        coordinate = Coordinate(0, 0)
+        for passabilities_row in area.passabilities:
             if result:
                 result += '\n'
-            for item in line:
-                if coordinate_from == item.coordinate:
+            for passability in passabilities_row:
+                if coordinate_from == coordinate:
                     result += ' OO'
-                elif coordinate_to == item.coordinate:
+                elif coordinate_to == coordinate:
                     result += ' XX'
                 else:
                     path_item_found = None
                     for path_item in path:
-                        if path_item == item.coordinate:
+                        if path_item == coordinate:
                             path_item_found = path_item
                             break
 
                     if path_item_found:
                         result += '  *'
-                    elif item.passability == -1:
+                    elif passability == -1:
                         result += ' ##'
-                    elif item.passability == 1:
+                    elif passability == 1:
                         result += '  .'
                     else:
-                        result += '%3d' % item.passability
+                        result += '%3d' % passability
+
+                coordinate.x += 1
+
+            coordinate.x = 0
+            coordinate.y += 1
+
         return result
 
     class Node(object):
-        def __init__(self, parent, coordinate):
-            self.parent = parent
-            self.coordinate = coordinate
-            self.f = 0.0
-            self.g = 0.0
-            self.h = 0.0
+        def __init__(node, parent, coordinate):
+            node.parent = parent
+            node.coordinate = coordinate
+            node.f = 0.0
+            node.g = 0.0
+            node.h = 0.0
 
         def __eq__(node1, node2):
             return node1.coordinate == node2.coordinate
 
-        def generateChildren(node, map):
+        def generateChildren(node, area):
             children = []
-            if node.coordinate.x + 1 < map.widht:
-                item = map.__map__[node.coordinate.y][node.coordinate.x+1]
-                if item.passability != -1:
-                    children.append(Map.Node(node, item.coordinate))
-            if node.coordinate.x > 0:
-                item = map.__map__[node.coordinate.y][node.coordinate.x-1]
-                if item.passability != -1:
-                    children.append(Map.Node(node, item.coordinate))
-            if node.coordinate.y + 1 < map.heidth:
-                item = map.__map__[node.coordinate.y+1][node.coordinate.x]
-                if item.passability != -1:
-                    children.append(Map.Node(node, item.coordinate))
-            if node.coordinate.y > 0:
-                item = map.__map__[node.coordinate.y-1][node.coordinate.x]
-                if item.passability != -1:
-                    children.append(Map.Node(node, item.coordinate))
+            coordinates = [
+                Coordinate(node.coordinate.x+1, node.coordinate.y),
+                Coordinate(node.coordinate.x-1, node.coordinate.y),
+                Coordinate(node.coordinate.x, node.coordinate.y+1),
+                Coordinate(node.coordinate.x, node.coordinate.y-1)]
+            for coordinate in coordinates:
+                child = node.generateChild(coordinate, area)
+                if child:
+                    children.append(child)
             return children
 
-    def calcPath(self, coordinate_from, coordinate_to):
+        def generateChild(node, coordinate, area):
+            if coordinate.x < 0 or area.widht <= coordinate.x:
+                return None
+            if coordinate.y < 0 or area.height <= coordinate.y:
+                return None
 
-        root = Map.Node(None, coordinate_from)
+            if area.getPassability(coordinate) == -1:
+                return None
+
+            return Area.Node(node, coordinate)
+
+    def getPath(area, coordinate_from, coordinate_to):
+
+        root = Area.Node(None, coordinate_from)
         root.g = 0.0
         root.h = root.coordinate.getLength(coordinate_to)
         root.f = root.g + root.h
@@ -127,12 +124,12 @@ class Map(object):
             open_set.remove(best_node)
             closed_set.append(best_node)
 
-            children = best_node.generateChildren(self)
+            children = best_node.generateChildren(area)
             for child in children:
                 if child in closed_set:
                     continue
 
-                child.g = best_node.g + self.__map__[child.coordinate.y][child.coordinate.x].passability
+                child.g = best_node.g + area.getPassability(child.coordinate)
                 child.h = child.coordinate.getLength(coordinate_to)
                 child.f = child.g + child.h
 
@@ -150,8 +147,8 @@ class Map(object):
 
         return []
 
-map = Map()
-coordinate_from = Map.Coordinate(2, 3)
-coordinate_to = Map.Coordinate(9, 1)
-path = map.calcPath(coordinate_from, coordinate_to)
-print map.get_str(coordinate_from, coordinate_to, path)
+area = Area()
+coordinate_from = Coordinate(2, 3)
+coordinate_to = Coordinate(9, 1)
+path = area.getPath(coordinate_from, coordinate_to)
+print area.toString(coordinate_from, coordinate_to, path)
